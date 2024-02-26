da7454b76b70dfb64b7ff1193c4b8fdf95c1f274
Eduland1000 <153714961+Eduland1000@users.noreply.github.com>
2024-02-26 21:26:42 +0100

    camera commite

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,20 +6,14 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform playerTransform;
     public Vector3 offset;
-
     public Vector2 minCameraPosition;
     public Vector2 maxCameraPosition;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        if(playerTransform != null)
+        if(playerTransform !=null)
+        
         {
             Vector3 desiredPosition = playerTransform.position + offset;
 
@@ -27,7 +21,7 @@ public class CameraMovement : MonoBehaviour
             float clampY = Mathf.Clamp(desiredPosition.y, minCameraPosition.y, maxCameraPosition.y);
 
             Vector3 clampedPosition = new Vector3(clampX, clampY, desiredPosition.z);
-            
+
             transform.position = clampedPosition;
         }
     }
